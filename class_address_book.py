59c058fce1fc19f4d943c6449c15476f5ffe5ac0
Liliia <myhaliuklilia@gmail.com>
2022-06-15 02:06:59 +0300

    Added requested fixes

--- class_address_book.py
+++ class_address_book.py
@@ -14,14 +14,20 @@ class AdressBook:
         'colleagues' : {}
     }
 
+
     def search(self, contact_name):
         """Function search for an instance of the contact with passed name"""
-        print(self.address_book)
+
+        contact_name = contact_name.lower()
         for contact_group in self.address_book:
-            return self.address_book[contact_group].get(contact_name, "Contact doesn't exist.")
+            if self.address_book[contact_group].get(contact_name) is not None:
+                return self.address_book[contact_group].get(contact_name)
+        return "Contact doesn't exist."
+
 
     def browse(self):
         """Display all contacts from the address book (browse action)"""
+
         for key, value in self.address_book.items():
             # Print group of contacts
             print(f"{key}: ")
@@ -29,6 +35,7 @@ class AdressBook:
             for contact in value.values():
                 # Print all contacts in the current group
                 print(f" {contact}")
+
 
     def add_new_contact(self):
         """Add new contact of the address book (add action)"""
@@ -44,32 +51,71 @@ class AdressBook:
         new_contact = Contact(cont_name, cont_email, cont_adress, cont_phone_number)
 
         # Check correctnes of the entered group name
-        ERRORS = 0
-        while True:
+        for error in range(2):
             try:
                 # Add new contact to the address book
-                self.address_book[cont_group][new_contact.name] = new_contact
+                self.address_book[cont_group][new_contact.name.lower()] = new_contact
                 print(self.address_book)
                 print("Contact was added!")
                 break
-            except KeyError:
-                ERRORS += 1
-                # Check amount of errors
-                if ERRORS > 2:
-                    print("Contact wasn't added!")
-                    break
-                cont_group = input("Enter correct group!(friends, family, colleagues): ")          
+            except KeyError:              
+                cont_group = input("Enter correct group!(friends, family, colleagues): ")
+                continue
+        else:
+            # User riched possible amount of errors(3)
+            print("Contact wasn't added!")
+
+
+    def change_param(self, mod_contact, param):
+        """Function modifies passed parameter of the contact"""
+
+        mod_contact.param = input(f"Enter new {param}: ")
+        res = input(f'{param} was modified. Do you want to modify another field? (yes/no): ' )
+        return res
+
+
+    def change_name(self, mod_contact):
+        """Function modifies name of the contact"""
+
+        old_name = mod_contact.name.lower()
+        new_name = input("Enter new name: ")
+
+        # Change the key (name of the contact) in address book dict
+        for key in self.address_book.keys():
+
+            # Add contact with new name to the dict
+            if self.address_book[key].get(old_name) is not None:
+                self.address_book[key][new_name.lower()] = self.address_book[key].get(old_name)
+
+                # Delete item with old name of the contact
+                del self.address_book[key][old_name]
+                mod_contact.name = new_name
+                break
+
+        res = input('Name was modified. Do you want to modify another field? (yes/no): ' )
+        return res
+
+
+    # Dict for dispatcher for changing parameters of contact
+    mod_actions = {
+        'name' : change_name,
+        'email' : change_param,
+        'address': change_param,
+        'phone_number' : change_param 
+        }
+
 
     def modify_contact(self):
         """Modify contact from contact book (modify action)"""
+
         # Get the name of the contact for search
         mod_contact = input("Modify the contact (Enter name): ")
         # Search for the contact with name that user entered
-        res = self.search(mod_contact)
-       
+        mod_contact = self.search(mod_contact)
+
         # Check if the contact was found
-        if isinstance(res, Contact):
-            mod_contact = res
+        if isinstance(mod_contact, Contact):
+
             # Show all information of the found contact to the user
             print(mod_contact)
 
@@ -81,45 +127,23 @@ class AdressBook:
                 mod_attr = input("What field do you want to modify?: ")
                 # Modify name of the contact
                 if mod_attr == "name":                  
-                    new_name = input("Enter new name: ")
-
-                    # Change the key (name of the contact) in address book dict
-                    for key in self.address_book.keys():
-                        if self.address_book[key].get(mod_contact.name) is not None:
-                            # Add contact with new name to the dict
-                            self.address_book[key][new_name] = self.address_book[key].get(mod_contact.name)
-                            # Delete item with old name of the contact
-                            del self.address_book[key][mod_contact.name]
-                            mod_contact.name = new_name
-                            break
-
-                    USER_INPUT = input('Name was modified. Do you want to modified another field? (yes/no): ' )
+                    USER_INPUT = self.mod_actions.get(mod_attr)(self, mod_contact)
                     continue
-                # Modify email of the contact
-                elif mod_attr == "email":
-                    mod_contact.email = input("Enter new email: ")
-                    USER_INPUT = input('Email was modified. Do you want to modified another field? (yes/no): ' )
-                    continue
-                # Modify address of the contact
-                elif mod_attr == "address":
-                    mod_contact.address = input("Enter new address: ")
-                    USER_INPUT = input('Address was modified. Do you want to modified another field? (yes/no): ' )
-                    continue
-                # Modify phone number of the contact
-                elif mod_attr == "phone number":
-                    mod_contact.phone_number = input("Enter new phone number: ")
-                    USER_INPUT = input('Address was modified. Do you want to modified another field? (yes/no): ' )
+                # Modify email, address, phone_number of the contact
+                elif mod_attr in ["email", "address", "phone_number"]:
+                    USER_INPUT = self.mod_actions.get(mod_attr)(self, mod_contact, mod_attr)
                     continue
                 else:
                     print("Not valid field. Enter correct modified field!")
         else:
             # Contact wasn't found. Print >> Contact does't exist
-            print(res)
+            print(mod_contact)
+
 
     def delete_contact(self):
         """Delete a contact from address book"""
         # Get the name of the contact that has to be deleted
-        del_contact = input("Delete contact (Enter name): ")
+        del_contact = input("Delete contact (Enter name): ").lower()
         
         # Find a contact in the groups of contacts
         for key in self.address_book.keys():
@@ -129,7 +153,8 @@ class AdressBook:
                 break
         else:
             print("Contact doesn't exist.")
-  
+
+
     def search_contact(self):
         """Search a contact in address book"""
         s_name = input("Search for the contact (Enter name): ")
--- main_menu.py
+++ main_menu.py
@@ -12,16 +12,23 @@ from class_address_book import AdressBook
 # Check if the address_book file already exists
 
 ADDRESS_BOOK_PATH = os.path.sep.join(["address_book", "address_book.pkl"])
-print(ADDRESS_BOOK_PATH)
+
 if os.path.isfile(ADDRESS_BOOK_PATH):
     with open(ADDRESS_BOOK_PATH, 'rb') as f:
         new_address_book = pickle.load(f)
-        print(new_address_book)
-        print(new_address_book.address_book)
 else:
     print("Hello")
     # Create new address book
     new_address_book = AdressBook()
+    
+# List of possible actions for menu
+actions = {
+    "1" : new_address_book.browse,
+    "2" : new_address_book.add_new_contact,
+    "3" : new_address_book.modify_contact,
+    "4" : new_address_book.delete_contact,
+    "5" : new_address_book.search_contact,
+}
 
 # Main menu
 while True:
@@ -36,32 +43,17 @@ while True:
         0 - Exit
         Your action: ''')
 
-    # Display all contacts from the address book (browse action)
-    if action == "1":
-        new_address_book.browse()
-
-    # Add new contact of the address book (add action)
-    if action == "2":
-        new_address_book.add_new_contact()
-
-    # Modify contact from contact book (modify action)
-    if action == "3":
-        new_address_book.modify_contact()
-
-    # Delete a contact from address book
-    if action == "4":
-        new_address_book.delete_contact()
-  
-    # Search a contact in address book
-    if action == "5":
-        new_address_book.search_contact()
-
     # Exit from the programm
     if action == "0":
         break
+    # Find and do chosen action
+    try:
+        actions.get(action)()
+    except KeyError:
+        print("Action does't exist")
+        continue
 
 
 # Save address book for later retrieval
 with open(ADDRESS_BOOK_PATH, "wb+") as f:
     pickle.dump(new_address_book, f)
-    print(new_address_book.address_book)
